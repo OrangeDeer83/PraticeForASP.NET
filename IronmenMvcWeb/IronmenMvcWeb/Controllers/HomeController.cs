9843ccafa918a99852dd18e104bebf345aa1992c
Steven Liu <steven200083@gmail.com>
2020-09-14 22:13:43 +0800

    加入model binding

---
+++ IronmenMvcWeb/IronmenMvcWeb/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using IronmenMvcWeb.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace IronmenMvcWeb.Controllers
+{
+    public class CustomModelBInder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var values = bindingContext.ValueProvider.GetValue("abc");
+
+            //var value = Int32.Parse(values.FirstValue);
+            //if (values > 10)
+            bindingContext.Result = ModelBindingResult.Success(values + "10");
+            //else
+            //    bindingContext.Result = ModelBindingResult.Success(values + "100");
+
+            return Task.CompletedTask;
+        }
+    }
+    [Route("MyHome/[Action]")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Your application description page HiHi.";
+            Pokemon[] pokemon = new Pokemon[2];
+            pokemon[0] = new Pokemon()
+            {
+                Id = 1,
+                Name = "水箭龜",
+                Property = "水系"
+            };
+
+            pokemon[1] = new Pokemon()
+            {
+                Id = 2,
+                Name = "小火龍",
+                Property = "火系"
+            };
+            /*Test[] test = new Test[2];
+            test[0] = new Test() { ID = 1 };
+            test[1] = new Test() { ID = 2 };*/
+
+            return View(pokemon);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Route("SubContact")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Hello(Pokemon pokemon)
+        {
+            
+            
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ IronmenMvcWeb/IronmenMvcWeb/Models/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IronmenMvcWeb.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Property { get; set; }
+        public string Name { get; set; }
+    }
+}
